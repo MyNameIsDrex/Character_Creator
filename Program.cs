18805ab844e7d04187ffe406995d7d43163d930f
Rasmus Löberg <rasmus.loberg04@gmail.com>
2023-12-06 01:45:38 +0100

    Early configuring

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Character_Creator
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Form());
         }
     }
 }
