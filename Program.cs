acf742134fd120129e30d80ea2c07e69cec22f2c
Rasmus Löberg <rasmus.loberg04@gmail.com>
2023-12-19 02:30:38 +0100

    Reworked name_generator function

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Character_Creator.Properties;
 using Microsoft.VisualBasic;
 using System.Drawing.Text;
+using System.Security.Permissions;
 
 namespace Character_Creator
 {
@@ -21,69 +22,26 @@ namespace Character_Creator
 
         public static void name_generator()
         {
-            //List<string> male_names = new List<string> { "Steve" , "Bob" , "Eric" , "Dylan" , "Max"};
-            //List<string> female_names = new List<string> { "Cassandra" , "Hannah" , "Sylvia" , "Nelly"};
-            //string name;
-
-            //Random r = new Random();
+            List<string> prefixes_fantasy = new List<string> { "Cor", "Jan", "Aef", "Ory", "Sam", "Gan", "Win", "Und", "Ell", "Eca", "Mim", "Hoy", "Var", "Lib", "Sha"};
+            List<string> prefixes_highfantasy = new List<string> { "Aen", "Viu", "Ran", "Izze", "Paa", "Eif", "Drak", "Loy", "Uar", "Miu", "Veu", "Jin", "Eer", "Caa", "Uon"};
             
-            /*
-            name = male_names[r.Next(5)];
+            List<string> prefixes_future = new List<string> { "Dor", "Hil", "Xal", "Ylas", "Tex", "Mega", "Tron", "Exo", "Win", "Raxu", "Milo", "Ins", "Ion", "Vo", "U-Ca"};
+            List<string> prefixes_robot = new List<string> { "K-", "HAL", "91-", "XL", "19-", "Hyper", "Mega", "Giga", "Tera", "Exa", "EX-", "B98-", "TA-", "HXX", "XM88-", "P-", "PP-", "X", "VTGK", "JDC", "US", "RUS", "SWE", "CN", "TWA", "FIN", "ITA", "ARG", "rA9", "Bob"};
+
+            List<string> prefixes_asian = new List<string> { "Xing", "San", "Wuk", "Tao", "Tsu", "Woa", "Rei", "Kumi", "Oni", "Jon", "Lem", "Puk", "Han", "Wei", "Miu", "Yao", "Bushi", "Son", "Tzen", "Tei", "Tui", "Budo", "Xom", "Wim"};
+
+            List<string> prefixes_leader = new List<string> { "Cpt ", "Master ", "Captain ", "Operator ", "Liutenant ", "Leader ", "Lt. ", "Op. "};
+            
+            string name = "";
+
+            Random r = new Random();
+            
             if (name == Settings.Default.name)
             {
-                name = male_names[r.Next(5)];
+                name_generator();
             }
             Settings.Default.name = name;
-            */
-        }
 
-        class name_info
-        {
-            public string Name { get; }
-            public Gender Gender { get; }
-            public Theme Theme { get; }
-            public Profession Profession { get; }
-
-            public name_info(string name, Gender gender, Theme theme, Profession profession)
-            {
-                Name = name;
-                Gender = gender;
-                Theme = theme;
-                Profession = profession;
-            }
-        }
-
-        static List<name_info> namesDatabase = new List<name_info>
-        {
-            //male names
-        new name_info("Martimus", Gender.Male, Theme.Fantasy, Profession.Warrior),
-        new name_info("Alex", Gender.Male, Theme.Fantasy, Profession.Wizard),
-        //new name_info("")
-
-
-            //female names
-        new name_info("Emily", Gender.Female, Theme.Fantasy, Profession.Wizard),
-        
-        };
-
-        enum Gender
-        {
-            Male,
-            Female
-        }
-
-        enum Theme
-        {
-            Fantasy,
-            // Add more themes as needed
-        }
-
-        enum Profession
-        {
-            None,
-            Warrior,
-            Wizard,
-            // Add more professions as needed
         }
     }
 } 
